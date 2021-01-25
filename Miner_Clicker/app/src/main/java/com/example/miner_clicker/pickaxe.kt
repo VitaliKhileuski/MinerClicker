bf3625c4944870af0987189f4371783857b14534
Vitali Khileuski <kentkentkent28@gmail.com>
2021-01-25 23:22:07 +0300

    create pickaxe

---
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/pickaxe.kt
@@ -0,0 +1,10 @@
+package com.example.miner_clicker
+
+class pickaxe() {
+    val id:Int=0;
+    val name:String=""
+    val price:Int=0
+    val descriprion:String=""
+    val damage:Int=0
+    val imageSource:String=""
+}
