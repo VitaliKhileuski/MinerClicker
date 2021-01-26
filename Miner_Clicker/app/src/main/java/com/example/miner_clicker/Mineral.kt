787123d2ef06d9034d8a15f50cf6681e5c7a4a56
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-01-26 18:35:48 +0300

    small changes with code view

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/Mineral.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/Mineral.kt
@@ -1,6 +1,6 @@
 package com.example.miner_clicker
 
-open class Mineral(Name:String,IconSource:String) {
-      val name=Name
-      val iconSource=IconSource
+open class Mineral(Name: String, IconSource: String) {
+    val name = Name
+    val iconSource = IconSource
 }
--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/StorageItem.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/StorageItem.kt
@@ -1,7 +1,7 @@
 package com.example.miner_clicker
 
 class StorageItem() {
-    var mineral:Mineral=Mineral()
-    var numberOfMineral:Int=0
+    var mineral: Mineral = Mineral("stone", "")
+    var numberOfMineral: Int = 0
 
 }
