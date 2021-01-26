74ee905dedebf5a99592e14558395100e6dfaaf2
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-01-26 19:00:08 +0300

    added drill fragment to tabs

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/MainActivity.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import com.example.miner_clicker.adapters.ViewPagerAdapter
+import com.example.miner_clicker.fragments.DrillFragment
 import com.example.miner_clicker.fragments.ShopFragment
 import com.example.miner_clicker.fragments.StorageFragment
 import com.google.android.material.tabs.TabLayout
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         val adapter=ViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(ShopFragment(),"Shop")
         adapter.addFragment(StorageFragment(),"storage")
+        adapter.addFragment(DrillFragment(), "Drill")
         viewPager = findViewById(R.id.viewPager)
         viewPager.adapter = adapter
         tabs = findViewById(R.id.tabs)
@@ -31,5 +33,6 @@ class MainActivity : AppCompatActivity() {
 
         tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_shopping_cart_24)
         tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_storage_24)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_construction_24)
     }
 }
