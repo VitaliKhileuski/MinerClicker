535e484e8ca5d01ea2d535c5da35efd53b1eb964
Vitali Khileuski <kentkentkent28@gmail.com>
2021-01-27 16:42:10 +0300

    create GridLayout

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/StorageItem.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/StorageItem.kt
@@ -1,7 +1,9 @@
 package com.example.miner_clicker
 
 class StorageItem() {
-    var mineral: Mineral = Mineral("stone", "")
+    var mineral: Mineral = Mineral("", "")
     var numberOfMineral: Int = 0
+    var capacity:Int=0
+
 
 }
