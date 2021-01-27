29303c0ecaf7452df6b88a04d6f7d18e3d207132
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-01-27 16:50:58 +0300

    StorageAdapter, item_view

---
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/adapters/StorageRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.miner_clicker.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.miner_clicker.R
+import com.example.miner_clicker.StorageItem
+
+class StorageRecyclerAdapter(private var storageItems: List<StorageItem>) : RecyclerView.Adapter<StorageRecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val resourceName: TextView = itemView.findViewById(R.id.resource_name)
+        val resourceAmount: TextView = itemView.findViewById(R.id.resource_amount)
+        val resourceImage: ImageView = itemView.findViewById(R.id.resource_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.storage_view_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.resourceName.text = storageItems[position].mineral.name
+        holder.resourceAmount.text = storageItems[position].numberOfMineral.toString()
+        //holder.resourceImage.setImageResource(storageItems[position].mineral.iconSource)
+    }
+
+    override fun getItemCount(): Int {
+        return storageItems.count()
+    }
+
+}
