c66c452f80d713e0d98d8f120055d3b1028668ec
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-01-30 02:07:42 +0300

    progressBar for Ore

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
@@ -5,12 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainGameActionFragmentVM:ViewModel() {
+
     private var money= MutableLiveData<Int>()
     public var Money: LiveData<Int> = money
+    private var currentHpOre = MutableLiveData<Int>()
+    public var CurrentHpOre: LiveData<Int> = currentHpOre
+    private var maxHpOre = MutableLiveData<Int>()
+    public var MaxHpOre : LiveData<Int> = maxHpOre
+
     init {
         money.value = 0
+        maxHpOre.value = 10000
+        currentHpOre.value = maxHpOre.value
     }
     fun Click(){
         money.value=money.value!!.plus(1)
+        currentHpOre.value = currentHpOre.value!!.minus(1000)
+        if(currentHpOre.value!! <= 0){
+            maxHpOre.value = maxHpOre.value!!.plus(5000)
+            currentHpOre.value = maxHpOre.value
+        }
     }
 }
