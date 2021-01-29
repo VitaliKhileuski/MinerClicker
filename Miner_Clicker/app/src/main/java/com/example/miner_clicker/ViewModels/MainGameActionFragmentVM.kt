c282ba1e66ae0ec067956ac50d54f39c387d5fc4
Vitali Khileuski <kentkentkent28@gmail.com>
2021-01-30 02:37:25 +0300

    connected ore  with viewModel

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
@@ -3,11 +3,12 @@ package com.example.miner_clicker.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.miner_clicker.models.Ore
 
 class MainGameActionFragmentVM:ViewModel() {
-
-    private var money= MutableLiveData<Int>()
-    public var Money: LiveData<Int> = money
+    private var ore:Ore = Ore(10,5,1)
+    private var money = MutableLiveData<Int>()
+    public var Money : LiveData<Int> = money
     private var currentHpOre = MutableLiveData<Int>()
     public var CurrentHpOre: LiveData<Int> = currentHpOre
     private var maxHpOre = MutableLiveData<Int>()
@@ -15,15 +16,19 @@ class MainGameActionFragmentVM:ViewModel() {
 
     init {
         money.value = 0
-        maxHpOre.value = 10000
-        currentHpOre.value = maxHpOre.value
+        maxHpOre.value =ore.maxHealth
+        currentHpOre.value =ore.maxHealth
     }
     fun Click(){
         money.value=money.value!!.plus(1)
-        currentHpOre.value = currentHpOre.value!!.minus(1000)
+        currentHpOre.value = currentHpOre.value!!.minus(1)
         if(currentHpOre.value!! <= 0){
-            maxHpOre.value = maxHpOre.value!!.plus(5000)
-            currentHpOre.value = maxHpOre.value
+            ore.SetNewLevel()
+            RefreshProperties()
         }
+    }
+    private fun RefreshProperties(){
+        maxHpOre.value =ore.maxHealth
+        currentHpOre.value =ore.maxHealth
     }
 }
--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Ore.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Ore.kt
@@ -1,15 +1,13 @@
 package com.example.miner_clicker.models
 
-class Ore() {
-    var health:Int=0
-    var capacity:Int=0
-    var level:Int=0
+class Ore(var maxHealth:Int=0,var capacity:Int=0,var level:Int=0) {
+
+
 
 
     public fun SetNewLevel(){
         level+=1
-        capacity+=25
-        health=capacity
+        maxHealth+=25
     }
 
 }
