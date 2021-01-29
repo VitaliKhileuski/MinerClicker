0f281bbb8a883f1a93e9817b9e8608e8e4662f6f
Vitali Khileuski <kentkentkent28@gmail.com>
2021-01-30 02:05:48 +0300

    Update Ore.kt

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Ore.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Ore.kt
@@ -1,6 +1,15 @@
 package com.example.miner_clicker.models
 
 class Ore() {
-    val health:Int=0
-    val capacity:Int=0
+    var health:Int=0
+    var capacity:Int=0
+    var level:Int=0
+
+
+    public fun SetNewLevel(){
+        level+=1
+        capacity+=25
+        health=capacity
+    }
+
 }
