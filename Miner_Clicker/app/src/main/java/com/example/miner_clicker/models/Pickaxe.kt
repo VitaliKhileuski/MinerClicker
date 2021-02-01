1afc0a9cc2cf90b2e725a61bfa17e4382a0d00d6
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-02-01 23:53:52 +0300

    small changes

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Pickaxe.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/models/Pickaxe.kt
@@ -3,10 +3,10 @@ package com.example.miner_clicker.models
 import com.example.miner_clicker.R
 
 class Pickaxe() {
-    val id:Int=0;
-    val name:String="none"
-    val price:Int=0
-    val descriprion:String="none"
-    val damage:Int=0
-    val imageSource:Int= R.drawable.ic_baseline_account_balance_24
+    var id:Int=0;
+    var name:String="none"
+    var price:Int=0
+    var descriprion:String="none"
+    var damage:Int=0
+    var imageSource:Int= R.drawable.ic_baseline_account_balance_24
 }
