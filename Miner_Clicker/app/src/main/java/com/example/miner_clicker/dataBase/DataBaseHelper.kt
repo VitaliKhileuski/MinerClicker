7639525c62dc292e930a7fc54b7c5049fd3c9fb4
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-02-02 03:32:38 +0300

    the first creating db for app

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/dataBase/DataBaseHelper.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/dataBase/DataBaseHelper.kt
@@ -1,8 +1,14 @@
 package com.example.miner_clicker.dataBase
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.miner_clicker.R
+import com.example.miner_clicker.dataBase.tables.OreTable
+import com.example.miner_clicker.dataBase.tables.PlayerTable
+import com.example.miner_clicker.dataBase.tables.ShopTable
+import com.example.miner_clicker.dataBase.tables.StorageTable
 
 class DataBaseHelper(context : Context): SQLiteOpenHelper(context,DataBaseInfo.DATABASE_NAME,
         null,DataBaseInfo.DATABASE_VERSION) {
@@ -12,6 +18,29 @@ class DataBaseHelper(context : Context): SQLiteOpenHelper(context,DataBaseInfo.D
         db?.execSQL(DataBaseInfo.CREATE_TABLE_STORAGE)
         db?.execSQL(DataBaseInfo.CREATE_TABLE_ORE)
         db?.execSQL(DataBaseInfo.CREATE_TABLE_SHOP)
+        db?.insert(PlayerTable.TABLE_NAME,null, ContentValues().apply { put(PlayerTable.COLUMN_NAME_MONEY, 0)
+            put(PlayerTable.COLUMN_NAME_GEMS, 0) })
+        db?.insert(StorageTable.TABLE_NAME,null, ContentValues().apply { put(StorageTable.COLUMN_NAME_MINERAL, "stone")
+            put(StorageTable.COLUMN_NAME_AMOUNT, 0)
+            put(StorageTable.COLUMN_NAME_CAPACITY, 200) })
+        db?.insert(StorageTable.TABLE_NAME,null, ContentValues().apply { put(StorageTable.COLUMN_NAME_MINERAL, "iron")
+            put(StorageTable.COLUMN_NAME_AMOUNT, 0)
+            put(StorageTable.COLUMN_NAME_CAPACITY, 50) })
+        db?.insert(StorageTable.TABLE_NAME,null, ContentValues().apply { put(StorageTable.COLUMN_NAME_MINERAL, "gold")
+            put(StorageTable.COLUMN_NAME_AMOUNT, 0)
+            put(StorageTable.COLUMN_NAME_CAPACITY, 25) })
+        db?.insert(StorageTable.TABLE_NAME,null, ContentValues().apply { put(StorageTable.COLUMN_NAME_MINERAL, "diamond")
+            put(StorageTable.COLUMN_NAME_AMOUNT, 0)
+            put(StorageTable.COLUMN_NAME_CAPACITY, 10) })
+        db?.insert(OreTable.TABLE_NAME,null, ContentValues().apply { put(OreTable.COLUMN_NAME_LEVEL, 1)
+            put(OreTable.COLUMN_NAME_MAX_HEALTH, 10)
+            put(OreTable.COLUMN_NAME_CURRENT_HEALTH, 10)
+            put(OreTable.COLUMN_NAME_CAPACITY, 5) })
+        db?.insert(ShopTable.TABLE_NAME,null, ContentValues().apply { put(ShopTable.COLUMN_NAME_PRODUCT_NAME, "pick")
+            put(ShopTable.COLUMN_NAME_PRODUCT_DESCRIPTION, "some description")
+            put(ShopTable.COLUMN_NAME_PRODUCT_PRICE, 10)
+            put(ShopTable.COLUMN_NAME_PRODUCT_DAMAGE, 5)
+            put(ShopTable.COLUMN_NAME_PRODUCT_SOURCE, R.drawable.ic_baseline_account_balance_24) })
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
