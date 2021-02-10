6bafce37d0607d66848a08876e27fa4a1079adf8
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-02-10 22:37:03 +0300

    Update MainGameActionFragmentVM.kt

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/ViewModels/MainGameActionFragmentVM.kt
@@ -1,7 +1,6 @@
 package com.example.miner_clicker.viewModels
 
 
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -16,84 +15,78 @@ import com.example.miner_clicker.models.gameComponents.Player
 import com.example.miner_clicker.models.gameComponents.RandomMinerals
 import com.example.miner_clicker.models.gameComponents.Storage
 
-class MainGameActionFragmentVM(var database : DataBase, var storageFragment: StorageFragment):ViewModel() {
-    private var player : Player = database.ReadPlayerData()
-    private var ore : Ore = database.ReadPOreData()
-    private var storage : Storage = database.ReadStorageData()
-
+class MainGameActionFragmentVM(var database: DataBase, var storageFragment: StorageFragment) : ViewModel() {
+    private var player: Player = database.ReadPlayerData()
+    private var ore: Ore = database.ReadPOreData()
+    private var storage: Storage = database.ReadStorageData()
     private var money = MutableLiveData<Int>()
+    private var stringMoney = MutableLiveData<String>()
     private var gems = MutableLiveData<Int>()
-
     private var capacity = MutableLiveData<Int>()
     private var level = MutableLiveData<Int>()
     private var maxHpOre = MutableLiveData<Int>()
     private var currentHpOre = MutableLiveData<Int>()
-
     private var textProgressBar = MutableLiveData<String>()
 
-    public var TextProgressBar : LiveData<String> = textProgressBar
-    public var Money : LiveData<Int> = money
+    public var TextProgressBar: LiveData<String> = textProgressBar
+    public var Money: LiveData<Int> = money
     public var CurrentHpOre: LiveData<Int> = currentHpOre
-    public var MaxHpOre : LiveData<Int> = maxHpOre
-    public var Gems : LiveData<Int> = gems
-    public var Capacity : LiveData<Int> = capacity
-    public var Level : LiveData<Int> = level
-
-    public var flag : Boolean = false
-    private var stringMoney = MutableLiveData<String>()
-    public var StringMoney : LiveData<String> = stringMoney
-
+    public var MaxHpOre: LiveData<Int> = maxHpOre
+    public var Gems: LiveData<Int> = gems
+    public var Capacity: LiveData<Int> = capacity
+    public var Level: LiveData<Int> = level
+    public var flag: Boolean = false
+    public var StringMoney: LiveData<String> = stringMoney
 
     init {
         money.value = player.money
-        if(money.value!!>=1000){
-            stringMoney.value =ConvertBigNumbers.ToString(money.value!!)
-        }else{
-        stringMoney.value = money.value.toString()
+        if (money.value!! >= 1000) {
+            stringMoney.value = ConvertBigNumbers.ToString(money.value!!)
+        } else {
+            stringMoney.value = money.value.toString()
         }
-
-
         gems.value = player.gems
         RefreshProperties()
     }
-    fun Click(){
-        money.value=money.value!!.plus(1)
-        stringMoney.value=money.value.toString()
-        if(money.value!!>=1000){
-           stringMoney.value=ConvertBigNumbers.ToString(money.value!!)
-        }
 
+    fun Click() {
+        money.value = money.value!!.plus(1)
+        stringMoney.value = money.value.toString()
+        if (money.value!! >= 1000) {
+            stringMoney.value = ConvertBigNumbers.ToString(money.value!!)
+        }
         currentHpOre.value = currentHpOre.value!!.minus(player.currentPickaxe.damage)
         ore.currenthealth = currentHpOre.value!!
         database.UpdateOreCurrentHealth(ore.currenthealth)
-        textProgressBar.value=currentHpOre.value.toString()+"/"+maxHpOre.value.toString()
-        if(currentHpOre.value!! <= 0){
+        textProgressBar.value = currentHpOre.value.toString() + "/" + maxHpOre.value.toString()
+        if (currentHpOre.value!! <= 0) {
             createNewOre()
             UpdateStorageRecyclerView()
         }
     }
-    private fun RefreshProperties(){
-        maxHpOre.value =ore.maxHealth
-        currentHpOre.value =ore.currenthealth
-        capacity.value=ore.capacity
-        textProgressBar.value=currentHpOre.value.toString()+"/"+maxHpOre.value.toString()
+
+    private fun RefreshProperties() {
+        maxHpOre.value = ore.maxHealth
+        currentHpOre.value = ore.currenthealth
+        capacity.value = ore.capacity
+        textProgressBar.value = currentHpOre.value.toString() + "/" + maxHpOre.value.toString()
     }
-    fun createNewOre(){
 
-        RandomMinerals.getStorageItems(storage,ore.capacity)
-
+    fun createNewOre() {
+        RandomMinerals.getStorageItems(storage, ore.capacity)
         ore.SetNewLevel()
         UpdateDatabase()
         RefreshProperties()
 
     }
-    private fun UpdateDatabase(){
+
+    private fun UpdateDatabase() {
         database.UpdateStorage(storage)
         database.UpdateMoney(money.value!!)
         database.UpdateOre(ore)
     }
 
-    private fun UpdateStorageRecyclerView(){
+    private fun UpdateStorageRecyclerView() {
         var test = storageFragment.view?.findViewById<RecyclerView>(R.id.storage_recyclerView)
         test?.adapter = StorageRecyclerAdapter(database.ReadStorageData().storageItems)
     }
