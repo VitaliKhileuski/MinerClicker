02ad517174949c91d37a8a038b6e41fbe879ba87
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-02-12 11:54:21 +0300

    Update ConvertBigNumbers.kt

--- Miner_Clicker/app/src/main/java/com/example/miner_clicker/adapters/ConvertBigNumbers.kt
+++ Miner_Clicker/app/src/main/java/com/example/miner_clicker/adapters/ConvertBigNumbers.kt
@@ -1,36 +1,37 @@
 package com.example.miner_clicker.adapters
+
 import java.math.*
 import kotlin.math.pow
 
 class ConvertBigNumbers {
-    companion object{
-       val Letters : MutableList<String> = mutableListOf("k","m","b","t","q")
-        var letter : String = "a"
-        var stringNumber : String =""
-        var preDotNumber : Double = 0.0
-        var afterDotNumber : String = ""
+    companion object {
+        private val letters: MutableList<String> = mutableListOf("k", "m", "b", "t", "q")
+        private var letter: String = "a"
+        private var stringNumber: String = ""
+        private var preDotNumber: Double = 0.0
+        private var afterDotNumber: String = ""
 
-        fun ToString(number : Int) : String{
-            letter= getLetter(number)
-            afterDotNumber = if(preDotNumber<10){
-                number.toString()[1]+ number.toString()[2].toString()
+        fun ToString(number: Int): String {
+            letter = getLetter(number)
+            afterDotNumber = if (preDotNumber < 10) {
+                number.toString()[1] + number.toString()[2].toString()
             } else {
-                number.toString()[2]+number.toString()[3].toString()
+                number.toString()[2] + number.toString()[3].toString()
             }
-            stringNumber= preDotNumber.toInt().toString()+"."+ afterDotNumber+ letter
-
+            stringNumber = preDotNumber.toInt().toString() + "." + afterDotNumber + letter
             return stringNumber
         }
-        fun getLetter(number : Int) : String{
-            var _number : Int = number
-            var index : Int =0
-            while(_number>=1000){
-                _number/=1000
+
+        private fun getLetter(number: Int): String {
+            var tempNumber: Int = number
+            var index = 0
+            while (tempNumber >= 1000) {
+                tempNumber /= 1000
                 index++
             }
-            val k : Double = 1000.0
-            preDotNumber=number.toDouble()/(k.pow(index))
-            return Letters[index-1]
+            val k = 1000.0
+            preDotNumber = number.toDouble() / (k.pow(index))
+            return letters[index - 1]
         }
     }
 }
